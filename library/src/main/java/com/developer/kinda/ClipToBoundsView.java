78280c8218b62a07c02cb2890e3412e357307944
TutorialsAndroid <akshaysunilmasram@yahoo.com>
2019-06-10 16:17:34 +0530

    Updated Dependencies

---
+++ library/src/main/java/com/developer/kinda/ClipToBoundsView.java
@@ -0,0 +1,56 @@
+package com.developer.kinda;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+class ClipToBoundsView extends RelativeLayout {
+
+    private final Context context;
+    private Float cornerRadius;
+    private final Rect rect = new Rect();
+    private final RectF rectF = new RectF();
+    private final Path clipPath = new Path();
+
+    public ClipToBoundsView(Context context) {
+
+        super(context);
+        this.context = context;
+        init();
+    }
+
+    public ClipToBoundsView(Context context, AttributeSet attrs) {
+
+        super(context, attrs);
+        this.context = context;
+        init();
+    }
+
+    public ClipToBoundsView(Context context, AttributeSet attrs, int defStyleAttr) {
+
+        super(context, attrs, defStyleAttr);
+        this.context = context;
+        init();
+    }
+
+    private void init() {
+        this.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        cornerRadius = context.getResources().getDimension(R.dimen.corner_radius);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        canvas.getClipBounds(rect);
+        rectF.set(rect);
+        clipPath.reset();
+        clipPath.addRoundRect(rectF, cornerRadius, cornerRadius, Path.Direction.CW);
+        canvas.clipPath(clipPath);
+        super.onDraw(canvas);
+    }
+}
---
+++ library/src/main/java/com/developer/kinda/CompositeListener.java
@@ -0,0 +1,27 @@
+package com.developer.kinda;
+
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class CompositeListener implements View.OnFocusChangeListener {
+
+    private final List<View.OnFocusChangeListener> registeredListeners = new ArrayList<>();
+
+    void registerListener (View.OnFocusChangeListener listener) {
+        registeredListeners.add(listener);
+    }
+
+    void clearListeners() {
+        registeredListeners.clear();
+    }
+
+    @Override
+    public void onFocusChange(View view, boolean b) {
+
+        for(View.OnFocusChangeListener listener:registeredListeners) {
+            listener.onFocusChange(view, b);
+        }
+    }
+}
